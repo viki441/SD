1e641ddb3038f2009005ce19b5ba4aab355809f6
Viki <91424361+viki441@users.noreply.github.com>
2026-01-06 09:43:05 +0200

    Update MyString.h

--- sem4/implementations/MyString.h
+++ sem4/implementations/MyString.h
@@ -1,69 +1,40 @@
 #pragma once
+#include "MyVector.h"
 #include <stdexcept>
-#include <fstream>
 #include <ostream>
+#include <iostream>
 
-using namespace std;
-
-class MyString
+class MyString 
 {
-
 public:
+    MyString();
+    MyString(const char* s);
 
-	MyString();
-	MyString(const char* s);
-	
-	//copy
-	MyString(const MyString& other);
-	MyString& operator=(const MyString& other);
+    MyString(const MyString& other);
+    MyString& operator=(const MyString& other);
 
-	//move
-	MyString(MyString&& other) noexcept;
-	MyString& operator=(MyString&& other) noexcept;
+    MyString(MyString&& other) noexcept;
+    MyString& operator=(MyString&& other) noexcept;
 
-	//dtor
-	~MyString();
+    ~MyString() = default;
 
-	//getters and setters
-	const size_t getSize() const;
-	const char* getString() const;
+    size_t getSize() const;
+    const char* getString() const;
 
-	//predefined operators	
-	char& operator[](size_t index);
-	const char& operator[](size_t index) const;
-	
-	bool operator==(const MyString& other) const;
-	bool operator!=(const MyString& other) const;
+    char& operator[](size_t index);
+    const char& operator[](size_t index) const;
+    bool operator==(const MyString& other) const;
+    bool operator!=(const MyString& other) const;
 
+    void push_back(char c);
+    char pop_back();
+    bool isEmpty() const;
+    void clear();
+    bool equalsIgnoreCase(const MyString& other) const;
 
-	//additional
-	void push_back(const char c);
-	void reserve(size_t newCapacity);
-	char pop_back();
-	const bool isEmpty() const;
-	bool equalsIgnoreCase(const MyString& other) const;
-	friend ostream& operator<<(ostream& os, const MyString& str);
-	
-	static bool isLetter(const char ch);
-	static bool isDigit(const char ch);
-	static bool isOperator(const char ch);
-	
-	//void print() const;
-
+    friend std::ostream& operator<<(std::ostream& os, const MyString& str);
 
 private:
-
-	char* string;
-	size_t size;
-	size_t capacity;
-	int growthFactor = 2;
-
-	void freeDynamic();
-	void copyDynamic(const MyString& other);
-
-	void resize(const int growthFactor);
-	//void shrinkToSize();
+    MyVector<char> data;
+    //void ensureNullTerminator();
 };
-
-
-
