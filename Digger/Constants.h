da1f88778f30e3451ae96b4b72d8a4e7937e2fef
Viki <91424361+viki441@users.noreply.github.com>
2026-02-07 11:16:34 +0200

    Delete Digger directory

--- Digger/Constants.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr int TILE_SIZE = 32;
-constexpr int HERO_WIDTH = TILE_SIZE;
-constexpr int HERO_HEIGHT = TILE_SIZE;
-constexpr int ENEMY_WIDTH = TILE_SIZE;
-constexpr int ENEMY_HEIGHT = TILE_SIZE;
