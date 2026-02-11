c9ae562deec3cedfb3c59f2bd97ab3cea01a2cbc
Viki <91424361+viki441@users.noreply.github.com>
2026-02-11 18:52:31 +0200

    Delete Digger directory

--- Digger/TextureManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Game.h"
-
-class TextureManager
-{
-public:
-	static SDL_Texture* loadTexture(const char* fileName);
-	static void draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest);
-};
